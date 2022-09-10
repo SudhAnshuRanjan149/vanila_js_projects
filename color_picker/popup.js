776492bf6f156048a42accb88ac70b2090165558
Sudhanshu Ranjan <sudhanshuranjan149@gmail.com>
2022-09-10 13:01:43 +0530

    resubmitting after removing unnesessary permissions

--- color_picker/popup.js
+++ color_picker/popup.js
@@ -1,16 +1,36 @@
 const btn = document.querySelector(".changeColorBtn");
+const colorGrid = document.querySelector(".colorGrid");
+const colorValue = document.querySelector(".colorValue");
 
 btn.addEventListener("click", async () => {
+	chrome.storage.sync.get('color',({color}) => {
+		console.log('color: ', color);
+	})
+
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
   console.log(tab);
 
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: pickColor,
-  },async() => {
+  chrome.scripting.executeScript(
+    {
+      target: { tabId: tab.id },
+      function: pickColor,
+    },
+    async (injectionResults) => {
+      const [data] = injectionResults;
 
-  });
+      if (data.result) {
+        const color = data.result.sRGBHex;
+        colorGrid.style.backgroundColor = color;
+        colorValue.innerHTML = color;
+        try {
+          await navigator.clipboard.writeText(color);
+        } catch (err) {
+          console.error(err);
+        }
+      }
+    }
+  );
 });
 
 async function pickColor() {
